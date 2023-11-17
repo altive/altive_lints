a62449d6cab3d1dcbbf9b2f9f5b2b51d47ba0edb
Ryunosuke Muramatsu <muramatsu@altive.co.jp>
2023-11-17 12:04:44 +0900

    Fix file path in diffscrape command

--- diffscrape/cmd/rules/main.go
+++ diffscrape/cmd/rules/main.go
@@ -9,7 +9,7 @@ import (
 func main() {
 	diff, err := diffscrape.Scrape(
 		"https://dart.dev/tools/linter-rules/all",
-		"../packages/altive_lints/lib/all_lint_rules.yaml",
+		"../../../packages/altive_lints/lib/all_lint_rules.yaml",
 		"pre code.yaml",
 		"    - ",
 		true,
