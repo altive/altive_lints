9a7d21155499ca8ffd15c44ea5e170016ebbff01
naipaka <naipakapaka@gmail.com>
2024-07-27 16:40:38 +0900

    feat: Add `avoid_hardcoded_color` rule

---
+++ packages/altive_lints/lint_test/lints/avoid_hardcoded_color.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class MyWidget extends StatelessWidget {
+  const MyWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        // expect_lint: avoid_hardcoded_color
+        const ColoredBox(color: Color(0xFF00FF00)),
+        // expect_lint: avoid_hardcoded_color
+        const ColoredBox(color: Color.fromRGBO(0, 255, 0, 1)),
+        // expect_lint: avoid_hardcoded_color
+        const ColoredBox(color: Colors.green),
+        ColoredBox(color: Theme.of(context).colorScheme.primary),
+      ],
+    );
+  }
+}
