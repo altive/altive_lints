ac57d755e66fabd10d271b4073ef9dd8b7f29924
Ryota Kobayashi <45661924+naipaka@users.noreply.github.com>
2024-07-29 17:51:04 +0900

    feat: Add `avoid_single_child` rule (#30)
    * feat: Add `avoid_single_child` rule
    
    * chore: Add whitespace adjacent strings

---
+++ packages/altive_lints/lint_test/lints/avoid_single_child.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+class MyWidget extends StatelessWidget {
+  const MyWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      children: [
+        // expect_lint: avoid_single_child
+        const Column(
+          children: [
+            Text('Hello'),
+          ],
+        ),
+        // expect_lint: avoid_single_child
+        const Row(
+          children: [
+            Text('World'),
+          ],
+        ),
+        // expect_lint: avoid_single_child
+        const Flex(
+          direction: Axis.horizontal,
+          children: [
+            Text('Hello'),
+          ],
+        ),
+        // expect_lint: avoid_single_child
+        const Wrap(
+          children: [
+            Text('World'),
+          ],
+        ),
+        // expect_lint: avoid_single_child
+        ListView(
+          children: const [
+            Text('Hello'),
+          ],
+        ),
+        // expect_lint: avoid_single_child
+        SliverList.list(
+          children: const [
+            Text('World'),
+          ],
+        ),
+      ],
+    );
+  }
+}
