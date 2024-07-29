6321b08e786246f846895bcf8c83d27c88f92305
naipaka <naipakapaka@gmail.com>
2024-07-29 17:47:51 +0900

    feat: Add `avoid_single_child` rule

---
+++ packages/altive_lints/lib/src/lints/avoid_single_child.dart
@@ -0,0 +1,77 @@
+import 'package:analyzer/dart/ast/ast.dart';
+import 'package:analyzer/error/listener.dart';
+import 'package:collection/collection.dart';
+import 'package:custom_lint_builder/custom_lint_builder.dart';
+
+/// An `avoid_single_child` rule that warns against using layout
+/// widgets intended for multiple children with only one child.
+///
+/// This includes widgets like `Column`,`Row`, `Flex`, `Wrap`,
+/// `ListView`, and `SliverList`.
+///
+/// Using these widgets with a single child can lead to
+/// unnecessary overhead and less efficient layouts.
+/// Instead, consider using a widget more suited for single
+/// children or adding more children to the widget.
+///
+/// ### Example
+///
+/// #### BAD:
+///
+/// ```dart
+/// Column(
+///   children: <Widget>[YourWidget()], // LINT
+/// );
+/// ```
+///
+/// #### GOOD:
+///
+/// ```dart
+/// Center(child: YourWidget());
+/// // or
+/// Column(
+///   children: <Widget>[YourWidget1(), YourWidget2()],
+/// );
+/// ```
+class AvoidSingleChild extends DartLintRule {
+  const AvoidSingleChild() : super(code: _code);
+
+  static const _code = LintCode(
+    name: 'avoid_single_child',
+    problemMessage:
+        'Avoid using a single child in widgets that expect multiple children. '
+        'Consider using a single child widget or adding more children.',
+  );
+
+  @override
+  void run(
+    CustomLintResolver resolver,
+    ErrorReporter reporter,
+    CustomLintContext context,
+  ) {
+    context.registry.addInstanceCreationExpression((node) {
+      final className =
+          node.staticType?.getDisplayString(withNullability: false);
+      if ([
+        'Column',
+        'Row',
+        'Flex',
+        'Wrap',
+        'ListView',
+        'SliverList',
+      ].contains(className)) {
+        final childrenArg = node.argumentList.arguments.firstWhereOrNull(
+          (arg) => arg is NamedExpression && arg.name.label.name == 'children',
+        );
+
+        final childrenList = childrenArg is NamedExpression
+            ? childrenArg.expression as ListLiteral
+            : null;
+
+        if (childrenList != null && childrenList.elements.length == 1) {
+          reporter.reportErrorForNode(_code, node);
+        }
+      }
+    });
+  }
+}
