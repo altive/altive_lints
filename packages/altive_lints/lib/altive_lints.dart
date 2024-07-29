43e6dae2f276cabe78ee43e9a7c96b2b6e7e96f5
naipaka <naipakapaka@gmail.com>
2024-07-29 09:14:20 +0900

    feat: Add `prefer_dedicated_media_query_methods` rule

--- packages/altive_lints/lib/altive_lints.dart
+++ packages/altive_lints/lib/altive_lints.dart
@@ -4,6 +4,7 @@ import 'src/lints/avoid_hardcoded_color.dart';
 import 'src/lints/avoid_hardcoded_japanese.dart';
 import 'src/lints/avoid_shrink_wrap_in_list_view.dart';
 import 'src/lints/avoid_single_child.dart';
+import 'src/lints/prefer_dedicated_media_query_methods.dart';
 
 PluginBase createPlugin() => _AltivePlugin();
 
@@ -14,5 +15,6 @@ class _AltivePlugin extends PluginBase {
         const AvoidHardcodedJapanese(),
         const AvoidShrinkWrapInListView(),
         const AvoidSingleChild(),
+        const PreferDedicatedMediaQueryMethods(),
       ];
 }
