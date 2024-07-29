b36d165bb48408f16ad977683f702fb747f56bb7
Ryota Kobayashi <45661924+naipaka@users.noreply.github.com>
2024-07-29 14:29:08 +0900

    feat: Add `avoid_shrink_wrap_in_list_view` rule (#28)
    * feat: Add `avoid_shrink_wrap_in_list_view` rule
    
    * chore: Add doc comment
    
    * chore: Add examples of correct usage comment
    
    Co-authored-by: Ryunosuke Muramatsu <muramatsu@altive.co.jp>
    
    ---------
    
    Co-authored-by: Ryunosuke Muramatsu <muramatsu@altive.co.jp>

--- packages/altive_lints/lib/altive_lints.dart
+++ packages/altive_lints/lib/altive_lints.dart
@@ -1,6 +1,7 @@
 import 'package:custom_lint_builder/custom_lint_builder.dart';
 
 import 'src/lints/avoid_hardcoded_japanese.dart';
+import 'src/lints/avoid_shrink_wrap_in_list_view.dart';
 
 PluginBase createPlugin() => _AltivePlugin();
 
@@ -8,5 +9,6 @@ class _AltivePlugin extends PluginBase {
   @override
   List<LintRule> getLintRules(CustomLintConfigs configs) => [
         const AvoidHardcodedJapanese(),
+        const AvoidShrinkWrapInListView(),
       ];
 }
---
+++ packages/altive_lints/lib/src/lints/avoid_shrink_wrap_in_list_view.dart
@@ -0,0 +1,87 @@
+import 'package:analyzer/dart/ast/ast.dart';
+import 'package:analyzer/error/listener.dart';
+import 'package:collection/collection.dart';
+import 'package:custom_lint_builder/custom_lint_builder.dart';
+
+import '../utils/types_utils.dart';
+
+/// An `avoid_shrink_wrap_in_list_view` rule that discourages
+/// using `shrinkWrap` with `ListView`.
+///
+/// This property causes performance issues by requiring
+/// the list to fully layout its content upfront.
+/// Instead of `shrinkWrap`, consider using slivers
+/// for better performance with large lists.
+///
+/// ### Example
+///
+/// #### BAD:
+///
+/// ```dart
+/// ListView(
+///   shrinkWrap: true, // LINT
+///   children: <Widget>[
+///     Text('Hello'),
+///     Text('World'),
+///   ],
+/// );
+/// ```
+///
+/// #### GOOD:
+///
+/// ```dart
+/// CustomScrollView(
+///   slivers: <Widget>[
+///     SliverList.list(
+///       children: [
+///         Text('Hello'),
+///         Text('World'),
+///       ],
+///     ),
+///   ],
+/// );
+/// ```
+class AvoidShrinkWrapInListView extends DartLintRule {
+  const AvoidShrinkWrapInListView() : super(code: _code);
+
+  static const _code = LintCode(
+    name: 'avoid_shrink_wrap_in_list_view',
+    problemMessage: 'Avoid using ListView with shrinkWrap, '
+        'since it might degrade the performance.\n'
+        'Consider using slivers instead.'
+        'Or, it is originally intended to be used for shrinking '
+        'when there is room for height in a dialog, for example.',
+  );
+
+  @override
+  void run(
+    CustomLintResolver resolver,
+    ErrorReporter reporter,
+    CustomLintContext context,
+  ) {
+    context.registry.addInstanceCreationExpression((node) {
+      if (isListViewWidget(node.staticType) &&
+          _hasShrinkWrap(node) &&
+          _hasParentList(node)) {
+        reporter.reportErrorForNode(_code, node);
+      }
+    });
+  }
+
+  bool _hasShrinkWrap(InstanceCreationExpression node) =>
+      node.argumentList.arguments.firstWhereOrNull(
+        (arg) => arg is NamedExpression && arg.name.label.name == 'shrinkWrap',
+      ) !=
+      null;
+
+  bool _hasParentList(InstanceCreationExpression node) =>
+      node.thisOrAncestorMatching(
+        (parent) =>
+            parent != node &&
+            parent is InstanceCreationExpression &&
+            (isListViewWidget(parent.staticType) ||
+                isColumnWidget(parent.staticType) ||
+                isRowWidget(parent.staticType)),
+      ) !=
+      null;
+}
