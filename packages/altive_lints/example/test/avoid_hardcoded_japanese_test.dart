9bf0862e34d729b99d42dbc18e48159c8a43cb76
tsukuda <jukiya8891@gmail.com>
2024-10-29 17:46:47 +0900

    fix: Correction of directory location

---
+++ packages/altive_lints/example/test/avoid_hardcoded_japanese_test.dart
@@ -0,0 +1,9 @@
+// Check the `avoid_hardcoded_japanese` rule.
+//
+// It should exclude warnings for the entire `/test/` directory.
+
+const hiragana = 'あいうえお';
+
+const katakana = 'アイウエオ';
+
+const kanji = '漢字';
