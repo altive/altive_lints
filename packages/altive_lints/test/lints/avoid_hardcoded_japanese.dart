62d3bcdc710ed810e1145d92a1dfce16c6676c07
tsukuda <jukiya8891@gmail.com>
2024-10-29 13:51:47 +0900

    fix: Changed from lint_test to test

--- packages/altive_lints/test/lints/avoid_hardcoded_japanese.dart
+++ packages/altive_lints/test/lints/avoid_hardcoded_japanese.dart
@@ -1,12 +1,9 @@
 // Check the `avoid_hardcoded_japanese` rule.
 
-// expect_lint: avoid_hardcoded_japanese
 const hiragana = 'あいうえお';
 
-// expect_lint: avoid_hardcoded_japanese
 const katakana = 'アイウエオ';
 
-// expect_lint: avoid_hardcoded_japanese
 const kanji = '漢字';
 
 const notJapanese = 'abc';
