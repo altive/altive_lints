46229730b44e8177ce7308d6048215c66de95b1c
tsukuda <jukiya8891@gmail.com>
2024-11-01 16:28:40 +0900

    fix: Define in colorscheme only the colors to be used.

--- packages/altive_lints/example/lints/avoid_hardcoded_color.dart
+++ packages/altive_lints/example/lints/avoid_hardcoded_color.dart
@@ -20,22 +20,6 @@ class MyWidget extends StatelessWidget {
     );
   }
 }
-ColorScheme get _colorScheme => const ColorScheme(
+ColorScheme get _colorScheme => const ColorScheme.dark(
       primary:Color.fromRGBO(0, 255, 0, 1),
-      secondary: Color(0xFFF45479),
-      onSecondary: Color(0xFFFFFFFF),
-      error: Color(0xFFFF3131),
-      surface: Color(0xFFFEFEFE),
-      surfaceDim: Color(0x1F787880),
-      surfaceTint: Color(0xFFF5F5F5),
-      surfaceContainerLowest: Color(0xFFF7F7F7),
-      surfaceContainerLow: Color(0xFFFAFBFC),
-      surfaceContainerHighest: Color(0xFFFFFFFF),
-      onSurface: Color(0xFF181A1F),
-      onSurfaceVariant: Color(0xFF989898),
-      outline: Color(0xFFDCDCDC),
-      scrim: Colors.black54,
-      onPrimary: Color.fromRGBO(100, 100, 100, 11),
-      onError: Color(0xFFFF3131),
-      brightness: Brightness.dark,
     );
